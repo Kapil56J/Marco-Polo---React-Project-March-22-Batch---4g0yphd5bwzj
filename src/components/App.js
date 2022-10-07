62d1e5eaf60945eee9206bc502fa630207fe9bb0
Kapil Jadhav <103952813+Kapil56J@users.noreply.github.com>
2022-10-07 21:30:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ const App = () => {
   
   return (
     <div id="main">
-    <h1 id="marco-polo">{alternate ? "Marco" : "Polo"}</h1>
+    <h1 id="marco-polo">{alternate ? "Polo" : "Marco"}</h1>
     <button id="marco-polo-toggler" onClick={Handler}>{alternate ? "Marco" : "Polo"}</button>
     </div>
   )
